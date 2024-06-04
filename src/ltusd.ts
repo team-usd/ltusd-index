9a5362bd04673306b6e12af0dde4a7f04279e1d6
Aaron <sunweirong32@gmail.com>
2024-06-04 14:37:39 +0800

    add rolelist and blacklist

--- src/ltusd.ts
+++ src/ltusd.ts
@@ -1,76 +1,100 @@
 import {   
   Transfer as TransferEvent,
+  RoleGranted as RoleGrantedEvent,
+  RoleRevoked as RoleRevokedEvent,
+  Blacklisted,
+  UnBlacklisted
 } from "../generated/LTUSD/LTUSD"
-import { TotalSupply, Transfer} from "../generated/schema"
+import { TotalSupply, Transfer, Account, TotalTransfer} from "../generated/schema"
 import {
   fetchAccount,
-  fetchBalance
+  fetchBalance,
+  fetchTotal,
+  fetchRole,
+  fetchBlack,
 } from "./utils"
-import { Address } from "@graphprotocol/graph-ts"
+import { Address, BigInt, Bytes } from "@graphprotocol/graph-ts"
 import { LTUSD } from "../generated/LTUSD/LTUSD" 
 
+const zeroAddress = Address.fromString("0x0000000000000000000000000000000000000000");
+
 function handleMint(event: TransferEvent): void {
-  let zeroAddress = Address.fromString("0x0000000000000000000000000000000000000000")
   if (event.params.from.equals(zeroAddress) || event.params.to.equals(zeroAddress)) {
-    let totalSupply = new TotalSupply(event.transaction.hash.concatI32(event.logIndex.toI32()))
+    let totalSupply = new TotalSupply(event.transaction.hash.concatI32(event.logIndex.toI32()));
 
     let contract = LTUSD.bind(event.address)
-    let totalSupplyResult = contract.try_totalSupply()
+    let totalSupplyResult = contract.try_totalSupply();
     if (!totalSupplyResult.reverted) {
-      totalSupply.value = totalSupplyResult.value
-      totalSupply.blockNumber = event.block.number
-      totalSupply.blockTimestamp = event.block.timestamp.toI32()
-      totalSupply.save()
+      totalSupply.value = totalSupplyResult.value;
+      totalSupply.blockNumber = event.block.number;
+      totalSupply.blockTimestamp = event.block.timestamp.toI32();
+      totalSupply.save();
     }
   }
 }
 
 function handleTransferData(event: TransferEvent): void {
-  let entity = new Transfer("auto")
-  entity.from = event.params.from
-  entity.to = event.params.to
-  entity.value = event.params.value
+  let entity = new Transfer("auto");
+  entity.from = event.params.from;
+  entity.to = event.params.to;
+  entity.value = event.params.value;
 
-  entity.blockNumber = event.block.number
-  entity.timestamp = event.block.timestamp.toI32()
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.timestamp = event.block.timestamp.toI32();
+  entity.transactionHash = event.transaction.hash;
 
-  let zeroAddress = Address.fromString("0x0000000000000000000000000000000000000000")
+  let zeroAddress = Address.fromString("0x0000000000000000000000000000000000000000");
   if (event.params.from.equals(zeroAddress)) {
     entity.type = "MINT";
   } else if (event.params.to.equals(zeroAddress)) {
     entity.type = "BURN";
   } else {
     entity.type = "NORMAL";
+    
   }
 
-  entity.save()
+  entity.save();
 
+  if (entity.type === "NORMAL") {
+    let total = fetchTotal(event);
+    total.totalCount = total.totalCount.plus(BigInt.fromI32(1));
+    total.totalVolume = total.totalVolume.plus(event.params.value);
+    total.blockNumber = event.block.number;
+    total.blockTimestamp = event.block.timestamp.toI32();
+    total.save();
+  }
 }
 
 function handleBalance(event: TransferEvent, address: Address): void {
   //fetch account details
-  let account = fetchAccount(address.toHex())
+  let account = fetchAccount(address.toHex());
 
-  if (account) {
-    let balance = fetchBalance(event.address, address)
+  if (account !== null) {
+    let balance = fetchBalance(event.address, address);
     if (balance !== null) {
-      account.amount = balance
-      account.blockNumber = event.block.number
-      account.blockTimestamp = event.block.timestamp.toI32()
-      account.save()
+      account.amount = balance;
     }
+    if (address.equals(event.params.from) && !zeroAddress.equals(event.params.to)) {
+      account.outTx = account.outTx + 1;
+    }
+    if(address.equals(event.params.to) && !zeroAddress.equals(event.params.from)) {
+      account.inTx = account.inTx + 1;
+    }
+
+    account.blockNumber = event.block.number;
+    account.blockTimestamp = event.block.timestamp.toI32();
+    account.save();
   }
 }
 
 
 export function handleTransfer(event: TransferEvent): void {
-  handleMint(event)
+  handleMint(event);
 
   // let entity = new Transfer(
   //   event.transaction.hash.concatI32(event.logIndex.toI32()),
   // )
-  handleTransferData(event)
+  handleTransferData(event);
 
   // let token = fetchTokenDetails(event);
   // if (!token) { //if token == null
@@ -78,8 +102,8 @@ export function handleTransfer(event: TransferEvent): void {
   // }
 
   //get account addresses from event
-  handleBalance(event, event.params.from)
-  handleBalance(event, event.params.to)
+  handleBalance(event, event.params.from);
+  handleBalance(event, event.params.to);
 
   //setting the token balance of the 'from' account
   // let fromTokenBalance = TokenBalance.load(token.id + "-" + fromAccount.id);
@@ -111,4 +135,27 @@ export function handleTransfer(event: TransferEvent): void {
   // if(toTokenBalance.amount != BigDecimal.fromString("0")){
   //   toTokenBalance.save();
   // }
+}
+export function handleRoleGranted(event: RoleGrantedEvent): void {
+  let role = fetchRole(event.params.role, event.params.account);
+  role.enabled = true;
+  role.save();
+}
+
+export function handleRoleRevoked(event: RoleRevokedEvent): void {
+  let role = fetchRole(event.params.role, event.params.account);
+  role.enabled = false;
+  role.save();
+}
+
+export function handleBlacklisted(event: Blacklisted): void {
+  let black = fetchBlack(event.params._account);
+  black.enabled = true;
+  black.save();
+}
+
+export function handleUnBlacklisted(event: UnBlacklisted): void {
+  let black = fetchBlack(event.params._account);
+  black.enabled = false;
+  black.save();
 }
--- src/utils.ts
+++ src/utils.ts
@@ -1,13 +1,13 @@
 //import smart contract class from generated files
 import { LTUSD } from "../generated/LTUSD/LTUSD";
 //import entities
-import { Account, Token } from "../generated/schema";
+import { Account, Token, TotalTransfer, Role, Black } from "../generated/schema";
 //import datatypes
-import { BigDecimal, ethereum, Address, BigInt, Int8 } from "@graphprotocol/graph-ts";
+import { BigDecimal, ethereum, Address, BigInt, Int8, Bytes } from "@graphprotocol/graph-ts";
 //fetch token details
 // export function fetchTokenDetails(event: ethereum.Event): Token | null {
 //   //check if token details are already saved
-//   let token = Token.load(event.address.toHex());
+  // let token = Token.load(event.address.toHex());
 //   if (!token) {
 //     //if token details are not available
 //     //create a new token
@@ -45,11 +45,23 @@ import { BigDecimal, ethereum, Address, BigInt, Int8 } from "@graphprotocol/grap
 //   return token;
 // }
 
+export function fetchTotal(event: ethereum.Event): TotalTransfer {
+  //check if token details are already saved
+  let totalTransfer = TotalTransfer.load(event.address.toHex());
+  if (!totalTransfer) {
+    totalTransfer = new TotalTransfer(event.address.toHex());
+    totalTransfer.blockNumber = BigInt.zero();
+    totalTransfer.blockTimestamp = BigInt.zero().toI32();
+    totalTransfer.totalCount = BigInt.zero();
+    totalTransfer.totalVolume = BigInt.zero();
+  }
+  return totalTransfer;
+}
+
 //fetch account details
 export function fetchAccount(address: string): Account | null {
-  //check if account details are already saved
-  const zeroAddress = "0x0000000000000000000000000000000000000000"
-  if (zeroAddress === address) {
+  const zeroAddress = Address.fromString("0x0000000000000000000000000000000000000000");
+  if (zeroAddress.equals(Address.fromString(address))) {
     return null;
   }
   let account = Account.load(address);
@@ -58,6 +70,8 @@ export function fetchAccount(address: string): Account | null {
     //create new account
     account = new Account(address);
     account.amount = BigInt.zero();
+    account.inTx = 0;
+    account.outTx = 0;
     account.blockNumber = BigInt.zero();
     account.blockTimestamp = BigInt.zero().toI32();
     account.save();
@@ -80,4 +94,50 @@ export function fetchBalance(
     amount = tokenBalance.value;
   }
   return amount;
+}
+
+// export const MinterRole: string = "0x9f2df0fed2c77648de5860a4cc508cd0818c85b8b8a1ab4ceeef8d981c8956a6";
+
+
+// const roleMap  = {
+//   "0x0000000000000000000000000000000000000000000000000000000000000000": "DEFAULT_ADMIN_ROLE",
+//   "0x679bd9d3c888f23cc573720774e2f42add4a913e21d56de2f469b5f3e0b834e0": "BLACK_LISTER_ROLE",
+//   "0x32c33f5a95173f202aa9a300d4ba14c55156248d1b2356b88052fc439349ab5d": "MINTER_MASTER_ROLE",
+//   "0x9f2df0fed2c77648de5860a4cc508cd0818c85b8b8a1ab4ceeef8d981c8956a6": "MINTER_ROLE",
+//   "0x65d7a28e3265b37a6474929f336521b332c1681b933f6cb9f3376673440d862a": "PAUSER_ROLE",
+//   "0xcf6f9f892731e14b8859835f2ff35575f447fb501f46243c4eb8bac19e31a050": "RESCUER_ROLE",
+// };
+
+let roleMap  = new Map<string, string>();
+roleMap.set("0x0000000000000000000000000000000000000000000000000000000000000000", "DEFAULT_ADMIN_ROLE")
+roleMap.set("0x679bd9d3c888f23cc573720774e2f42add4a913e21d56de2f469b5f3e0b834e0", "BLACK_LISTER_ROLE")
+roleMap.set("0x32c33f5a95173f202aa9a300d4ba14c55156248d1b2356b88052fc439349ab5d", "MINTER_MASTER_ROLE")
+roleMap.set("0x9f2df0fed2c77648de5860a4cc508cd0818c85b8b8a1ab4ceeef8d981c8956a6", "MINTER_ROLE")
+roleMap.set("0x65d7a28e3265b37a6474929f336521b332c1681b933f6cb9f3376673440d862a", "PAUSER_ROLE")
+roleMap.set("0xcf6f9f892731e14b8859835f2ff35575f447fb501f46243c4eb8bac19e31a050", "RESCUER_ROLE")
+
+
+export function fetchRole(roleHash: Bytes, address: Address): Role {
+  
+  let role = Role.load(roleHash.toHex() + "-" + address.toHex());
+  if (!role) {
+    //create new Role
+    role = new Role(roleHash.toHex() + "-" + address.toHex());
+    role.roleName = roleMap.get(roleHash.toHex());
+    role.address = address;
+    role.enabled = false;
+    role.save()
+  }
+  return role;
+}
+
+export function fetchBlack(address: Address): Black {
+  let black = Black.load(address.toHex());
+  if (!black) {
+    //create new black
+    black = new Black(address.toHex());
+    black.enabled = false;
+    black.save();
+  }
+  return black;
 }
